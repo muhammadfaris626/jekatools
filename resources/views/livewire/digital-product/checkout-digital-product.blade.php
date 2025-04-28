f91ef7833d7707b02f57f96e006479c9f375b81d
muhammadfaris626 <muhammadfaris5795@gmail.com>
2025-04-28 23:10:26 +0800

    ww

--- resources/views/livewire/digital-product/checkout-digital-product.blade.php
+++ resources/views/livewire/digital-product/checkout-digital-product.blade.php
@@ -7,38 +7,36 @@
             <flux:breadcrumbs.item divider="slash">Checkout</flux:breadcrumbs.item>
         </flux:breadcrumbs>
         <div class="flex gap-4">
-            <flux:button :href="route('digital-product.index')" size="sm" variant="danger" icon="arrow-left">Kembali</flux:button>
+            <flux:button :href="route('digital-product.read', $show->id)" size="sm" variant="danger" icon="arrow-left">Kembali</flux:button>
         </div>
     </div>
     <flux:separator />
-    {{-- <div class="grid grid-cols-1 gap-4 my-4">
-        <div class="grid grid-cols-1 md:grid-cols-6 gap-2">
-            <div class="md:col-span-2 flex flex-col">
-                <div class="relative rounded-lg bg-white dark:bg-zinc-600 p-2">
-                    <img class="rounded-lg h-48 w-full object-cover" src="{{ asset('images/product3.png') }}" />
+    <div class="grid grid-cols-1 gap-4 mt-4">
+        <flux:heading size="lg" class="text-center">Pilih Metode Pembayaran Anda</flux:heading>
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+            @foreach($channels as $key => $value)
+                <form wire:submit.prevent="transaction('{{ $value['code'] }}')">
+                    @php
+                        $bankImages = [
+                            'BNIVA' => 'bni.png',
+                            'BRIVA' => 'bri.png',
+                            'MANDIRIVA' => 'mandiri.png',
+                            'BCAVA' => 'bca.png'
+                        ];
+                        $image = $bankImages[$value['code']] ?? 'default.png';
+                    @endphp
+                    <div class="rounded-lg bg-white dark:bg-zinc-600 p-1">
+                        <div class="rounded-lg p-4 bg-white ">
+                            <img class="h-16 w-full object-contain" src="{{ asset('images/bank/' . $image) }}" />
+                        </div>
+                        <flux:heading size="md" class="text-center mt-2">Pembayaran</flux:heading>
+                        <flux:heading size="md" class="text-center mb-2">{{ $value['name'] }}</flux:heading>
+                    </div>
                     <div class="mt-2">
-                        <flux:button :href="route('digital-product.checkout', $show->id)" variant="primary" class="w-full" icon="shopping-cart">Checkout</flux:button>
+                        <flux:button type="submit" variant="primary" size="sm" class="w-full">PILIH</flux:button>
                     </div>
-                </div>
-            </div>
-            <div class="md:col-span-4 flex flex-col">
-                <div class="relative flex-1 rounded-lg bg-white dark:bg-zinc-600 p-4">
-                    <div>
-                        <flux:badge color="sky">{{ ucfirst(strtolower($show->type)) }}</flux:badge>
-                    </div>
-                    <div class="mt-2">
-                        <flux:heading size="lg">{{ $show->name }}</flux:heading>
-                    </div>
-                    <div class="mt-2">
-                        <flux:heading>Harga</flux:heading>
-                        <flux:text size="xl">{{ 'Rp ' . number_format($show->price, 0, ',', '.') }}</flux:text>
-                    </div>
-                    <div class="mt-2">
-                        <flux:heading>Keterangan</flux:heading>
-                        <flux:text class="mt-2">{!! $show->desc !!}</flux:text>
-                    </div>
-                </div>
-            </div>
+                </form>
+            @endforeach
         </div>
-    </div> --}}
+    </div>
 </app>
